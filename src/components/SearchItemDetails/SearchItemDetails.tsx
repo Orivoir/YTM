1eeb83a77be0fa9c66edb6779fa8c261f14a59dc
Samuel Gaborieau <sam.gabor@hotmail.com>
2022-07-03 06:21:55 +0200

    upgrade: modal => details item, fix: laggy/jerky at open/close modal

--- src/components/SearchItemDetails/SearchItemDetails.tsx
+++ src/components/SearchItemDetails/SearchItemDetails.tsx
@@ -1,60 +1,67 @@
 import * as React from "react"
+import {DeviceEventEmitter} from "react-native"
 import { Modal, Portal, Surface } from "react-native-paper"
 import type { AlbumPreviewAPI, ArtistPreviewAPI, MusicVideoAPI } from "../../api/ytm-api"
-import { useAppDispatch, useAppSelector } from "../../hooks/redux"
-import { createCancel } from "../../store/actions/searchDetailsActions"
-import { SearchDetailsState } from "../../store/reducers/searchDetailsReducers"
 import AlbumDetails from "../AlbumDetails/AlbumDetails"
 import ArtistDetails from "../ArtistDetails/ArtistDetails"
 import MusicDetails from "../MusicDetails/MusicDetails"
 import styles from "../styles"
 
-interface SearchItemDetailsProps {
+interface SearchItemDetailsProps {}
 
+type SearchDetailsState = MusicVideoAPI | ArtistPreviewAPI | AlbumPreviewAPI | null;
+
+const isAlbum = (details: SearchDetailsState): details is AlbumPreviewAPI => (
+  !!(details as AlbumPreviewAPI)?.albumId
+)
+const isArtist = (details: SearchDetailsState): details is ArtistPreviewAPI => (
+  !!(details as ArtistPreviewAPI)?.artistId
+)
+const isMusic = (details: SearchDetailsState): details is MusicVideoAPI => (
+  !!(details as MusicVideoAPI)?.youtubeId
+)
+
+
+const getComponent = (details: SearchDetailsState | null = null): React.ElementType<{
+  onClose: () => void;
+  item: any
+}> | never => {
+  if (!details) {
+    return React.Fragment
+  }
+
+  if (isAlbum(details)) {
+    return AlbumDetails
+  } else if (isArtist(details)) {
+    return ArtistDetails
+  } else if (isMusic(details)) {
+    return MusicDetails
+  } else {
+    throw new Error("cant render SearchItemDetails because never find item category")
+  }
 }
 
 const SearchItemDetails: React.FC<SearchItemDetailsProps> = () => {
-  const searchDetails = useAppSelector(state => state.searchDetails)
-  const dispatch = useAppDispatch()
 
-  const isAlbum = (details: SearchDetailsState): details is AlbumPreviewAPI => (
-    !!(details as AlbumPreviewAPI)?.albumId
-  )
-  const isArtist = (details: SearchDetailsState): details is ArtistPreviewAPI => (
-    !!(details as ArtistPreviewAPI)?.artistId
-  )
-  const isMusic = (details: SearchDetailsState): details is MusicVideoAPI => (
-    !!(details as MusicVideoAPI)?.youtubeId
-  )
-
-  const getComponent = (details: SearchDetailsState | null = null): React.ElementType<{
-    onClose: () => void;
-    item: any
-  }> | never => {
-    if (!details) {
-      return React.Fragment
-    }
-
-    if (isAlbum(details)) {
-      return AlbumDetails
-    } else if (isArtist(details)) {
-      return ArtistDetails
-    } else if (isMusic(details)) {
-      return MusicDetails
-    } else {
-      throw new Error("cant render SearchItemDetails because never find item category")
-    }
-  }
-
-  const Component = getComponent(searchDetails)
+  const [searchDetails, setSearchDetails] = React.useState<SearchDetailsState>(null);
 
   const onCancelDetails = () => {
-    dispatch(createCancel())
+    setSearchDetails(null);
   }
 
-  if (!searchDetails) {
-    return <></>
+  const onSearchItemDetails = (item: MusicVideoAPI | ArtistPreviewAPI | AlbumPreviewAPI) => {
+    setSearchDetails(item);
   }
+
+  React.useEffect(() => {
+    const subscription = DeviceEventEmitter.addListener("search.item.details", onSearchItemDetails);
+
+    return () => {
+      subscription.remove();
+    }
+  }, []);
+
+  const Component = getComponent(searchDetails);
 
   return (
     <Portal>
