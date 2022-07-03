e1b61deb668e8cefb5ee2fa0141a5946a0ab422f
Samuel Gaborieau <sam.gabor@hotmail.com>
2022-07-03 21:46:07 +0200

    add helper file: constant.ts

---
+++ src/constant.ts
@@ -0,0 +1,6 @@
+export const EVENT_DOWNLOAD_PROGRESS_UPDATE = "progress.update";
+export const EVENT_SEARCH_EXEC = "search.exec";
+export const EVENT_SEARCH_ITEM_DETAILS = "search.item.details";
+
+export const AUDIO_FAST_OFFSET_TIME_DURATION = 10000; // 10 seconds
+export const AUDIO_FAST_OFFSET_BATCH_COULDOWN_DURATION = 500; // 0.5 seconds
