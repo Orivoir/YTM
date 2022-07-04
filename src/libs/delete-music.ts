09ad2c9c0ab91d2e10f54f8a558a0e0e249e140d
Samuel Gaborieau <sam.gabor@hotmail.com>
2022-07-04 20:45:08 +0200

    fix: delete-music.ts

--- src/libs/delete-music.ts
+++ src/libs/delete-music.ts
@@ -1,40 +1,35 @@
 import { SQLError, SQLResultSet, SQLTransaction, WebSQLDatabase } from "expo-sqlite";
 import deleteMusicFileSystem from "./delete-music-file-system";
 
-export default function deleteMusic(database: WebSQLDatabase, musicId: number): Promise<boolean> {
+export default function deleteMusic(
+  database: WebSQLDatabase,
+  musicId: number,
+  filename: string
+): Promise<boolean> {
 
   return new Promise<boolean>((
     resolve: (isDeleted: boolean) => void,
     reject: (error: SQLError) => void
   ): void => {
-    database.transaction(tx => {
-
-      tx.executeSql(
-        "SELECT filename FROM music WHERE id = ?",
-        [musicId],
-        (_: SQLTransaction, result: SQLResultSet) => {
-          deleteMusicFileSystem(result.rows._array[0].filename)
-          .then(() => {
-            tx.executeSql(
-              "DELETE FROM music WHERE id = ?",
-              [musicId],
-              (_: SQLTransaction, result: SQLResultSet) => {
-                resolve(result.rowsAffected === 1)
-              },
-              (_: SQLTransaction, error: SQLError) => {
-                reject(error)
-                return false;
-              }
-            )
-          })
-          .catch(reject)
-        },
-        (_: SQLTransaction, error: SQLError) => {
-          reject(error)
-          return false;
-        }
-      )
+    deleteMusicFileSystem(filename)
+    .then(() => {
+      console.log("DELETE FILE SYSTEM")
+      database.transaction(tx => {
+        tx.executeSql(
+          "DELETE FROM music WHERE id = ?",
+          [musicId],
+          (_: SQLTransaction, result: SQLResultSet) => {
+            console.log("DELETE SQLITE ENTRY")
+            resolve(result.rowsAffected === 1)
+          },
+          (_: SQLTransaction, error: SQLError) => {
+            reject(error)
+            return false;
+          }
+        )
+      })
     })
+    .catch(reject)
   })
 
 }
