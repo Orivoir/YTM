b7d3e7c281ad0d53fd49ab047dcd786bafb7db54
Samuel Gaborieau <sam.gabor@hotmail.com>
2022-07-04 21:05:44 +0200

    upgrade PlaylistItem for hooks useSwipeDelete

--- src/components/PlaylistItem/PlaylistItem.tsx
+++ src/components/PlaylistItem/PlaylistItem.tsx
@@ -1,15 +1,10 @@
-import { MaterialCommunityIcons } from "@expo/vector-icons"
 import * as React from "react"
-import { DeviceEventEmitter, View } from "react-native"
-import Swipable from "react-native-gesture-handler/Swipeable"
-import { Avatar, Card, IconButton, Surface, Text, useTheme } from "react-native-paper"
+import { View } from "react-native"
+import { Avatar, IconButton, Surface, Text } from "react-native-paper"
 import DatabaseContext from "../../Context/DatabaseContext"
-import { useAppSelector } from "../../hooks/redux"
-import getMusicsByPlaylist from "../../libs/get-musics-by-playlist"
+import useSwipeDelete from "../../hooks/useSwipeDelete"
 import splitText from "../../libs/splitText"
-import ConfirmAction from "../ConfirmAction/ConfirmAction"
 import PlaylistDetails from "../PlaylistDetails/PlaylistDetails"
-import SwipeTrash from "../SwipeTrash/SwipeTrash"
 import deletePlaylist from "./../../libs/delete-playlist"
 
 interface PlaylistItemProps {
@@ -23,54 +18,11 @@ const PlaylistItem: React.FC<PlaylistItemProps> = ({
 }) => {
   const database = React.useContext(DatabaseContext)
 
-  // const [musics, setMusics] = React.useState<{
-  //   playlist_id: number,
-  //   id: number,
-  //   title: string,
-  //   ownerName: string,
-  //   filename: string,
-  //   publishedAt?: string,
-  //   thumbnail?: string,
-  //   ownerThumbnail?: string
-  // }[]>([])
-
   const [isOpenDetails, setIsOpenDetails] = React.useState<boolean>(false)
-
-  // const download = useAppSelector(state => state.download);
-
-  const [isOpenConfirmDelete, setIsOpenConfirmDelete] = React.useState<boolean>(false)
 
   const thumbnailRef = React.useRef<string | null>(null)
 
   const isDeletedRef = React.useRef<boolean>(false);
-
-  const theme = useTheme()
-
-  const swipableRef = React.useRef<Swipable | null>(null);
-
-  // const refreshMusics = () => {
-  //   getMusicsByPlaylist(database, id)
-  //   .then(rows => {
-  //     rows._array.forEach(music => {
-  //       if (typeof music.thumbnail === "string") {
-  //         thumbnailRef.current = music.thumbnail
-  //       }
-  //     })
-
-  //     setMusics(rows._array)
-  //   })
-  //   .catch((sqlError) => {
-  //     console.log("> cant read musics from SQLite with: ", sqlError)
-  //   })
-  // }
-
-  // React.useEffect(() => {
-  //   refreshMusics();
-  // }, [id, download])
-
-  if(isDeletedRef.current) {
-    return <></>;
-  }
 
   const onOpenPlaylist = () => {
     setIsOpenDetails(true)
@@ -78,7 +30,7 @@ const PlaylistItem: React.FC<PlaylistItemProps> = ({
 
   const onDeletePlaylist = () => {
     isDeletedRef.current = true;
-    setIsOpenConfirmDelete(false);
+    // setIsOpenConfirmDelete(false);
 
     deletePlaylist(database, id)
     .then(countMusicsDeleted => {
@@ -91,52 +43,53 @@ const PlaylistItem: React.FC<PlaylistItemProps> = ({
     })
   }
 
-  return (
-    <>
-      <SwipeTrash
-        onGetSwipableRef={swipable => {
-          swipableRef.current = swipable
-        }}
-        contentAction="REMOVE"
-        contentActionIcon={(
-          <MaterialCommunityIcons name='trash-can' size={24} color={theme.colors.text} />
-        )}
-        onTrash={() => {
-          setIsOpenConfirmDelete(true)
-        }}>
-        <Surface style={{
-          paddingVertical: 2,
-          paddingHorizontal: 4
+  const {render} = useSwipeDelete({
+    onDelete: onDeletePlaylist,
+    itemType: "playlist",
+    relationalText: `Do you want remove playlist ${name} and all musics from this ?`,
+    children: (
+      <Surface style={{
+        paddingVertical: 2,
+        paddingHorizontal: 4
+      }}>
+        <View style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center"
         }}>
           <View style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center"
+            marginEnd: 8
           }}>
-            <View style={{
-              marginEnd: 8
-            }}>
-              {thumbnailRef.current
-                ? (
-                <Avatar.Image size={32} source={{ uri: thumbnailRef.current }} />
-                  )
-                : (
-                <Avatar.Text size={32} label={name.charAt(0)} />
-                  )}
-            </View>
-
-            <View style={{
-              marginEnd: 8
-            }}>
-              <Text>{splitText(name)}</Text>
-            </View>
-
-            <View>
-              <IconButton icon="folder-music" onPress={onOpenPlaylist} />
-            </View>
+            {thumbnailRef.current
+              ? (
+              <Avatar.Image size={32} source={{ uri: thumbnailRef.current }} />
+                )
+              : (
+              <Avatar.Text size={32} label={name.charAt(0)} />
+                )}
           </View>
-        </Surface>
-      </SwipeTrash>
+
+          <View style={{
+            marginEnd: 8
+          }}>
+            <Text>{splitText(name)}</Text>
+          </View>
+
+          <View>
+            <IconButton icon="folder-music" onPress={onOpenPlaylist} />
+          </View>
+        </View>
+      </Surface>
+    )
+  })
+
+  if(isDeletedRef.current) {
+    return <></>;
+  }
+
+  return (
+    <>
+      {render}
 
       <PlaylistDetails
         playlist={{name, id}}
@@ -144,34 +97,6 @@ const PlaylistItem: React.FC<PlaylistItemProps> = ({
         onClose={() => {
           setIsOpenDetails(false)
         }} />
-
-        <ConfirmAction
-          actionCancel={{
-            title: "cancel",
-            type: "safe",
-            onPress: () => {
-              swipableRef.current?.close()
-              setIsOpenConfirmDelete(false)
-            }
-          }}
-          actionContinue={{
-            title: "remove",
-            type: "unsafe",
-            onPress: () => {
-              onDeletePlaylist();
-            }
-          }}
-          // title={`playlist contains ${musics.length} musics`}
-          title={`playlist contains X musics`}
-          relationalText={`Do you want remove playlist ${name} and all musics from this ?`}
-          warnCantUndo
-          actionType='remove'
-          itemType='playlist'
-          onClose={() => {
-            swipableRef.current?.close()
-            setIsOpenConfirmDelete(false)
-          }}
-          open={isOpenConfirmDelete} />
     </>
   )
 }
