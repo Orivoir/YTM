afbf2b0c4edcab1547de6e29be4fa10ef8818634
Samuel Gaborieau <sam.gabor@hotmail.com>
2022-07-04 02:33:40 +0200

    upgrade helper file

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,8 @@
 export const EVENT_DOWNLOAD_PROGRESS_UPDATE = "progress.update";
 export const EVENT_SEARCH_EXEC = "search.exec";
 export const EVENT_SEARCH_ITEM_DETAILS = "search.item.details";
+export const EVENT_ADD_DOWNLOAD = "download.add.item"
+export const EVENT_ADD_DOWNLOADS = "download.add.items"
 
 export const AUDIO_FAST_OFFSET_TIME_DURATION = 10000; // 10 seconds
 export const AUDIO_FAST_OFFSET_BATCH_COULDOWN_DURATION = 500; // 0.5 seconds
