3f407db61f9c2f6ea684bb6010e68b3a09b12349
Samuel Gaborieau <sam.gabor@hotmail.com>
2022-07-05 03:46:00 +0200

    upgrade hook useSelectPlaylist

--- src/hooks/useSelectPlaylist.tsx
+++ src/hooks/useSelectPlaylist.tsx
@@ -1,33 +1,44 @@
 import * as React from "react"
-import SelectPlaylist from "../components/SelectPlaylist/SelectPlaylist"
+import { DeviceEventEmitter, EmitterSubscription } from "react-native";
+import { EVENT_SELECTED_PLAYLIST, EVENT_SELECT_PLAYLIST } from "../constant";
 
 export type UseSelectPlaylistOptions = {
   musicTitle: string;
 }
 
 export default function useSelectPlaylist (options: UseSelectPlaylistOptions) {
-  const [isOpen, setIsOpen] = React.useState<boolean>(false)
 
   const [playlist, setPlaylist] = React.useState<{id: number, name: string} | null>(null)
 
-  const onSelect = (_playlist: {id: number, name: string}) => {
-    setPlaylist(_playlist)
+  const playlistSelectedSubscriptionRef = React.useRef<EmitterSubscription | null>(null);
+
+  const onSelectedPlaylist = (params: {
+    playlist: {id: number, name: string}
+  }) => {
+    setPlaylist(params.playlist)
   }
 
-  const onOpen = () => (
-    setIsOpen(true)
-  )
+  const onOpen = () => {
+    playlistSelectedSubscriptionRef.current?.remove();
+    playlistSelectedSubscriptionRef.current = DeviceEventEmitter.addListener(
+      EVENT_SELECTED_PLAYLIST,
+      onSelectedPlaylist
+    )
 
-  const render = (
-    <SelectPlaylist
-      musicTitle={options.musicTitle}
-      onClose={() => setIsOpen(false)}
-      onSelect={onSelect}
-      open={isOpen} />
-  )
+    DeviceEventEmitter.emit(EVENT_SELECT_PLAYLIST, {
+      musicTitle: options.musicTitle || "",
+    })
+  }
+
+  React.useEffect(() => {
+
+    return () => {
+      playlistSelectedSubscriptionRef.current?.remove()
+    }
+
+  }, [])
 
   return {
-    render,
     playlist,
     onOpen
   }
