410ce139805a55025db8d11160aeee621c66ac38
Samuel Gaborieau <sam.gabor@hotmail.com>
2022-07-05 03:46:37 +0200

    upgrade: helper file for events name of SelectPlaylist component

--- src/constant.ts
+++ src/constant.ts
@@ -4,6 +4,8 @@ export const EVENT_SEARCH_ITEM_DETAILS = "search.item.details";
 export const EVENT_ADD_DOWNLOAD = "download.add.item"
 export const EVENT_ADD_DOWNLOADS = "download.add.items"
 export const EVENT_CREATE_PLAYLIST = "playlist.create"
+export const EVENT_SELECT_PLAYLIST = "playlist.select"
+export const EVENT_SELECTED_PLAYLIST = "playlist.selected"
 
 export const AUDIO_FAST_OFFSET_TIME_DURATION = 10000; // 10 seconds
 export const AUDIO_FAST_OFFSET_BATCH_COULDOWN_DURATION = 500; // 0.5 seconds
